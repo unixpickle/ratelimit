913a9d1699520dae0f1db6c71e73f8d705ac8af8
Alex Nichol <unixpickle@gmail.com>
2015-09-12 13:59:59 -0400

    get identifiers for HTTP remotes

---
+++ http.go
@@ -0,0 +1,62 @@
+package ratelimit
+
+import (
+	"net"
+	"net/http"
+	"strings"
+)
+
+const DefaultIPv6Bits = 64
+
+// HTTPRemoteNamer assigns unique IDs to the remote hosts of http requests.
+type HTTPRemoteNamer struct {
+	// IPv6Bits indicates how many bits of IPv6 addresses should be counted towards the remote
+	// address. If this is 0, the default value DefaultIPv6Bits will be used.
+	IPv6Bits int
+}
+
+// Name generates a unique ID for the source of the HTTP request.
+func (h HTTPRemoteNamer) Name(r *http.Request) string {
+	ipStr := rawIPFromRequest(r)
+	parsed := net.ParseIP(ipStr)
+	if parsed == nil || parsed.To4() != nil {
+		return ipStr
+	} else {
+		return h.encodeIPv6Binary(parsed)
+	}
+}
+
+func (h HTTPRemoteNamer) encodeIPv6Binary(address []byte) string {
+	res := make([]byte, h.IPv6Bits)
+	for bitIndex := 0; bitIndex < h.IPv6Bits; bitIndex++ {
+		byteIndex := bitIndex / 8
+		bitShift := uint(7 - (bitIndex % 8))
+		if address[byteIndex]&(1<<bitShift) == 0 {
+			res[bitIndex] = '0'
+		} else {
+			res[bitIndex] = '1'
+		}
+	}
+	return string(res)
+}
+
+func rawIPFromRemoteAddr(addr string) string {
+	if !strings.HasPrefix(addr, "[") {
+		// The address is "IPv4Address:port"
+		return strings.Split(addr, ":")[0]
+	}
+
+	// The address is "[IPv6Address]:port".
+	ipv6Addr := strings.Split(addr, "]")[0]
+	if len(ipv6Addr) < 1 {
+		panic("invalid remote address: " + addr)
+	}
+	return ipv6Addr[1:]
+}
+
+func rawIPFromRequest(r *http.Request) string {
+	if forwardHeader := r.Header.Get("X-Forwarded-For"); forwardHeader != "" {
+		return strings.Split(forwardHeader, ", ")[0]
+	}
+	return rawIPFromRemoteAddr(r.RemoteAddr)
+}
---
+++ http_test.go
@@ -0,0 +1,37 @@
+package ratelimit
+
+import (
+	"net/http"
+	"testing"
+)
+
+func nameForRemoteAddr(n HTTPRemoteNamer, addr string) string {
+	var req http.Request
+	req.RemoteAddr = addr
+	return n.Name(&req)
+}
+
+func nameForRemoteIP(n HTTPRemoteNamer, ip string) string {
+	var req http.Request
+	req.Header = http.Header{}
+	req.Header.Set("X-Forwarded-For", ip)
+	return n.Name(&req)
+}
+
+func TestHTTPRemoteNamer(t *testing.T) {
+	namer := HTTPRemoteNamer{34}
+	if nameForRemoteAddr(namer, "[2001:0db8:ac10:fe01::]:1234") !=
+		"0010000000000001000011011011100010" {
+		t.Error("unexpected result")
+	}
+	if nameForRemoteAddr(namer, "123.76.192.1:1234") != "123.76.192.1" {
+		t.Error("unexpected result")
+	}
+	if nameForRemoteIP(namer, "2001:0db8:ac10:fe01::") !=
+		"0010000000000001000011011011100010" {
+		t.Error("unexpected result")
+	}
+	if nameForRemoteIP(namer, "123.76.192.1") != "123.76.192.1" {
+		t.Error("unexpected result")
+	}
+}
